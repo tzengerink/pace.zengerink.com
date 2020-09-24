b9dd5a0656d840f2e39adc72b956d4d4241430af
Teun Zengerink <t.zengerink@gmail.com>
2020-09-24 11:14:32 +0200

    Initial setup of React app

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+interface AppProps {}
+
+interface AppState {}
+
+export default class App extends React.Component<AppProps, AppState> {
+  render() {
+    return (
+      <div className="app"></div>
+    );
+  }
+}
