a8bb87e967b45e5651cd612bd2e8c99a8c0081c6
tzengerink <tzengerink@users.noreply.github.com>
2020-09-28 15:53:02 +0000

    deploy: 46a81c91ff238fec1d90be0f1a2f86b50b0fca61

---
+++ precache-manifest.7495e7fc81d169306c2492578fa0c5bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f90d7430b1c63a17774d823dbc6372a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f6a3fb16e305433a90b",
+    "url": "/static/css/main.438c90ab.chunk.css"
+  },
+  {
+    "revision": "ca9752538e0f3fcf1995",
+    "url": "/static/js/2.f755423a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.f755423a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f6a3fb16e305433a90b",
+    "url": "/static/js/main.5ea6f44b.chunk.js"
+  },
+  {
+    "revision": "c1f6bf1d12cfd361a7f0",
+    "url": "/static/js/runtime-main.547b36ed.js"
+  }
+]);
