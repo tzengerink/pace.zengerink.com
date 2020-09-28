771f9aa9174223fe94347b2814c6a5f836af0f8c
Teun Zengerink <t.zengerink@gmail.com>
2020-09-28 17:18:34 +0200

    Adjusted styling and added background image

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Calculator from './components/Calculator/Calculator'
+import './App.scss'
 
 export default class App extends React.Component<{}, {}> {
   render() {
