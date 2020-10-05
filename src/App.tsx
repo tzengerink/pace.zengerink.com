d3418fc39dda5ca2724ce88c5c7e2b46b09a2e71
Teun Zengerink <t.zengerink@gmail.com>
2020-10-05 16:41:05 +0200

    Rewrite App to function component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
-import Calculator from './components/Calculator/Calculator'
+import Calculator from './features/Calculator/Calculator'
 import './App.scss'
 
-export default class App extends React.Component<{}, {}> {
-  render() {
-    return (
-      <div className="app">
-        <Calculator />
-      </div>
-    )
-  }
+const App = () => {
+  return (
+    <div className="app">
+      <Calculator />
+    </div>
+  )
 }
+
+export default App
